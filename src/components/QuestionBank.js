16bbc60b9e50a909beeb122abb812e1f969843fe
steve <stephen@stephenmucheru79@gmail.com>
2023-11-30 11:22:55 +0300

    all the components files

---
+++ src/components/QuestionBank.js
@@ -0,0 +1,36 @@
+// QuestionBank.js
+
+const qBank = [
+	{
+		id: 1,
+		question: "What is the capital of kenya?",
+		options: ["kisumu", "eldoret", "Thika", "Nairobi"],
+		answer: "Nairobi",
+	},
+	{
+		id: 2,
+		question: "What is the capital of Uganda?",
+		options: ["Entebe", "kampala", "Amritsar", "JInja"],
+		answer: "kampala",
+	},
+	{
+		id: 3,
+		question: "What is the capital of India?",
+		options: ["Delhi", "Mumbai", "Kolkata", "Chennai"],
+		answer: "Delhi"
+	},
+	{
+		id: 4,
+		question: "What is the capital of England?",
+		options: ["Mogadishu", "Paris", "London", "Naples"],
+		answer: "London"
+	},
+	{
+		id: 5,
+		question: "What is capital of  Italy?",
+		options: ["ManChester", "Rome", "Las vegas", "Madrid"],
+		answer: "Rome"
+	},
+]
+
+export default qBank;
